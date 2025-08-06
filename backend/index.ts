d555a0fbad88edf411ad779c8a20be027f45342c
Asaiy420 <kcaryan05@gmail.com>
2025-08-06 20:40:58 +0545

    feat: set up Express server with CORS and Food routes

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+import FoodRoutes from "./routes/food.route";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use("/api/food", FoodRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
